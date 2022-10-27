63a96ac755fcc7c2bf42e7489a089baf70281ac7
Hamid <hamidtahir2000@gmail.com>
2022-10-28 02:20:26 +0500

    Fixes

--- web/index.js
+++ web/index.js
@@ -204,6 +204,7 @@ Shopify.Webhooks.Registry.addHandler('FULFILLMENTS_UPDATE', {
     _body = JSON.parse(_body)
     console.log('Updated @ ' + shop)
     console.log(_body.shipment_status)
+    sendDataToKlaivyo(_body, shop)
     if (_body.shipment_status === 'delivered') {
       sendDataToKlaivyo(_body, shop)
     }
@@ -216,6 +217,7 @@ Shopify.Webhooks.Registry.addHandler('FULFILLMENTS_UPDATE', {
     _body = JSON.parse(_body)
     console.log('Updated @ ' + shop)
     console.log(_body.shipment_status)
+    sendDataToKlaivyo(_body, shop)
     if (_body.shipment_status === 'delivered') {
       sendDataToKlaivyo(_body, shop)
     }
